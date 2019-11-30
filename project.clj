53b159d81facf1542837de68dc972e74483201f0
João Santiago <joaocpsantiago@gmail.com>
2019-11-30 23:09:59 +0100

    starmen everywhere

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject starmen "0.1.0-SNAPSHOT"
+(defproject starmen "0.1.0"
   :description "Slack reminders to see the ISS in the sky"
   :url "http://starmen.herokuapp.com"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
@@ -19,6 +19,6 @@
   :plugins [[lein-cloverage "1.1.1"]
             [lein-cljfmt "0.6.4"]]
   :min-lein-version "2.0.0"
-  :uberjar-name "controltower.jar"
+  :uberjar-name "starmen.jar"
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
